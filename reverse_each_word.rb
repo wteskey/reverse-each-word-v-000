bad2ab3c0f908d612dffc3ac43227086e17c1d34
William J Teskey <willjteskey@gmail.com>
2018-04-27 21:51:18 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,2 +1,11 @@
-
+def reverse_each_word(sentence)
+  array = []
+  reversed_array = []
+  #sentence.reverse
+  array = sentence.split
+  array.each do |x|    
+    reversed_array << x.reverse
+  end
+  reversed_array.join(" ")
+end
   
