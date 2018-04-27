0aec92e39e9611f62a043d53c506ce3e331eb0fa
William J Teskey <willjteskey@gmail.com>
2018-04-27 21:52:46 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,2 @@
-def reverse_each_word(sentence)
-  words = sentence.split(" ")
-  reversed_words = []
-  words.each do |word|
-    reversed_words << word.reverse
-  end 
-  reversed_words.join(" ")
-end
+
   
